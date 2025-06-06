1060974156865223b808478e658bd43565bc7b5d
Erik Rodrigues <erik.webprogrammer@gmail.com>
2025-06-05 21:02:39 -0300

    add tasks into table

--- index.js
+++ index.js
@@ -10,5 +10,24 @@ const client = new Client({
     database: "todo"
 });
 
+const app = express();
+const port = 3000;
+
 client.connect().then(() => console.log("CONNECTED"));
 
+app.use(express.json());
+app.post("/add-task", (req, res) => {
+    const {creator, task} = req.body;
+    const insertQuery = "INSERT INTO todo (creator, task) VALUES ($1, $2)"
+
+    client.query(insertQuery, [creator, task], (err, result) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(result);
+        }
+    });
+
+});
+
+app.listen(port, () => console.log(`The app is running on http://localhost:${port}`));
